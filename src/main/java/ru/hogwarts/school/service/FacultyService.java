85a76d9684b0b7ed6524d64bcfd9076a80f6bb7a
Rafnes <rafzograf@gmail.com>
2024-12-05 17:01:47 +0300

    hwSQL_logging&config done

--- src/main/java/ru/hogwarts/school/service/FacultyService.java
+++ src/main/java/ru/hogwarts/school/service/FacultyService.java
@@ -1,5 +1,7 @@
 package ru.hogwarts.school.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.model.Faculty;
@@ -11,10 +13,11 @@ import java.util.*;
 
 @Service
 public class FacultyService {
-
     private final FacultyRepository facultyRepository;
 
     private final StudentRepository studentRepository;
+
+    Logger logger = LoggerFactory.getLogger(FacultyService.class);
 
     public FacultyService(FacultyRepository facultyRepository, StudentRepository studentRepository) {
         this.facultyRepository = facultyRepository;
@@ -22,36 +25,53 @@ public class FacultyService {
     }
 
     public Faculty createFaculty(Faculty faculty) {
+        logger.info("Creating faculty: {}", faculty.getName());
         return facultyRepository.save(faculty);
     }
 
     public Faculty findFaculty(long id) {
-        return facultyRepository.findById(id).get();
+        logger.info("Finding faculty with id: {}", id);
+        return facultyRepository.findById(id).orElseThrow(() -> {
+            logger.error("Faculty with id {} not found", id);
+            return new NoSuchElementException("Faculty not found");
+        });
     }
 
     public Faculty editFaculty(Faculty faculty) {
+        logger.info("Method editFaculty was invoked");
         return facultyRepository.save(faculty);
     }
 
     public void removeFaculty(long id) {
+        logger.info("Deleting faculty with id: {}", id);
+        if(!facultyRepository.existsById(id)) {
+            logger.warn("Faculty with id: {} not found, there is no entity to delete", id);
+        }
         facultyRepository.deleteById(id);
     }
 
     public Collection<Faculty> getAll() {
+        logger.debug("Getting all faculties");
         return facultyRepository.findAll();
     }
 
     public Collection<Student> getStudentsByFaculty(Long facultyId) {
+        logger.info("Method getStudentsByFaculty was invoked");
         Faculty faculty = facultyRepository.findById(facultyId)
-                .orElseThrow();
+                .orElseThrow(() -> {
+                    logger.error("Faculty with id: {} not found", facultyId);
+                    return new NoSuchElementException("Факультет не найден");
+                });
         return studentRepository.findAllByFacultyId(facultyId);
     }
 
     public Collection<Faculty> findFacultiesByColor(String color) {
+        logger.info("Finding faculties with color: {}", color);
         return facultyRepository.findByColorIgnoreCase(color);
     }
 
     public Collection<Faculty> findFacultiesByName(String name) {
+        logger.info("Finding faculties with name: {}", name);
         return facultyRepository.findByNameIgnoreCase(name);
     }
 }
--- src/main/java/ru/hogwarts/school/service/StudentService.java
+++ src/main/java/ru/hogwarts/school/service/StudentService.java
@@ -1,5 +1,7 @@
 package ru.hogwarts.school.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.model.Faculty;
@@ -20,47 +22,69 @@ public class StudentService {
     @Autowired
     FacultyRepository facultyRepository;
 
+    Logger logger = LoggerFactory.getLogger(StudentService.class);
+
     public Student createStudent(Student student, Long facultyId) {
-        Faculty faculty = facultyRepository.findById(facultyId).orElseThrow();
+        logger.info("Creating new student");
+        Faculty faculty = facultyRepository.findById(facultyId).orElseThrow(() -> {
+            logger.error("Faculty with id: {} not found", facultyId);
+            return new NoSuchElementException();
+        });
         student.setFaculty(faculty);
+        logger.debug("Faculty with id: {} set to created student. Saving student", facultyId);
         return studentRepository.save(student);
     }
 
     public Student findStudent(long id) {
+        logger.info("Method findStudent was invoked");
         return studentRepository.findById(id).get();
     }
 
     public Student updateStudent(Student student, Long facultyId) {
-        Faculty faculty = facultyRepository.findById(facultyId).orElseThrow();
+        logger.info("Method updateStudent was invoked");
+        Faculty faculty = facultyRepository.findById(facultyId).orElseThrow(() -> {
+            logger.error("Student with id: {} not found", facultyId);
+            return new NoSuchElementException();
+        });
         student.setFaculty(faculty);
         return studentRepository.save(student);
     }
 
     public void removeStudent(long id) {
+        logger.info("Removing student with id: {}", id);
+        if (!studentRepository.existsById(id)) {
+            logger.warn("Student with id: {} not found, there is no entity to delete", id);
+        }
         studentRepository.deleteById(id);
     }
 
     public Collection<Student> getAll() {
+        logger.info("Getting all students");
         return studentRepository.findAll();
     }
 
     public Collection<Student> findByAge(int age) {
+        logger.info("Method findByAge was invoked");
         return studentRepository.findByAge(age);
     }
 
     public List<Student> findByAgeBetween(int min, int max) {
+        logger.info("Method findByAgeBetween was invoked");
         return studentRepository.findByAgeBetween(min, max);
     }
 
     public int getStudentsAmount() {
+        logger.info("Method getStudentsAmount was invoked");
         return studentRepository.getStudentsAmount();
     }
 
     public double getAverageStudentAge() {
+        logger.info("Method getAverageStudentAge was invoked");
         return studentRepository.getAverageStudentAge();
     }
 
     public List<Student> getLastFiveStudents() {
+        logger.info("Method getLastFiveStudents was invoked");
         return studentRepository.getLastFiveStudents();
     }
 }
